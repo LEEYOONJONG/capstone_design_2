8e83253bfeeb5ef0b1829ac98c979c4037d3e73b
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-11 12:03:02 +0900

    Feat: 1초에 한번씩 눈깜빡임 인지하도록 처리

--- Blink/Blink/Views/ViewController.swift
+++ Blink/Blink/Views/ViewController.swift
@@ -16,7 +16,8 @@ final class ViewController: UIViewController {
     var contentNode: SCNReferenceNode?
     lazy var eyeLeftNode = contentNode?.childNode(withName: "eyeLeft", recursively: true)
     lazy var eyeRightNode = contentNode?.childNode(withName: "eyeRight", recursively: true)
-    
+    var blinked: Bool = false
+    var cnt:Int = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSceneView()
@@ -53,8 +54,24 @@ extension ViewController: ARSCNViewDelegate{
         let blendShapes = faceAnchor.blendShapes
         if let eyeBlinkLeft = blendShapes[.eyeBlinkLeft] as? Float,
            let eyeBlinkRight = blendShapes[.eyeBlinkRight] as? Float {
+            // scale : 1이면 눈 뜬 모양, 0이면 감은 모양
             eyeLeftNode?.scale.z = 1 - eyeBlinkLeft
             eyeRightNode?.scale.z = 1 - eyeBlinkRight
+            
+            blinkProcess(eyeBlinkLeft: eyeBlinkLeft, eyeBlinkRight: eyeBlinkRight)
+            
+        }
+    }
+    
+    private func blinkProcess(eyeBlinkLeft: Float, eyeBlinkRight: Float) {
+        if eyeBlinkLeft > 0.8 && eyeBlinkRight > 0.8 && !blinked {
+            print("\(cnt)번 감았음")
+            cnt += 1
+            blinked = true
+            // 1초에 한번씩만 눈깜빡임 인식
+            DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 1) {
+                self.blinked = false
+            }
         }
     }
 }
